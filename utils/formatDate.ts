af665e7a2aac186bb4dd122dc48fda7d9387f863
nandehu0323 <hiroki.nanjo@ginco.io>
2020-04-14 20:59:49 +0900

    :+1: Update: Kagawa Emergency Call

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -22,6 +22,24 @@ export const convertDateToISO8601Format = (dateString: string): string => {
   return dayjs(dateString).format('YYYY-MM-DD')
 }
 
+/**
+ * Get date string formatted follow each country's format(YYYY-MM-DD)
+ * China, Japan, Korea, Taiwan - YYYY-MM-DD
+ * US - MM/DD/YYYY
+ *
+ * @param dateString- Parsable string by dayjs
+ * @param locale- User's language
+ */
+export const convertDateByCountryPreferTimeFormat = (
+  dateString: string,
+  locale: string
+): string => {
+  if (locale === 'en') {
+    return dayjs(dateString).format('MMM DD, YYYY')
+  }
+  return dayjs(dateString).format('YYYY-MM-DD')
+}
+
 export const getLatestUpdateAt = (data: DataType[]): string => {
   return dayjs(data.reduce((a, b) => (a.date > b.date ? a : b)).date).format(
     'YYYY/MM/DD HH:mm'
