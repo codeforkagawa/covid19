bf0a5e2cc506491cf41d3f5c90e5e23c9812f701
nandehu0323 <hiroki.nanjo@ginco.io>
2020-04-18 03:17:08 +0900

    :wrench: Fix: 軽症・中等症・重症の欄を消す

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -1,5 +1,5 @@
 type DataType = {
-  attr: '検査実施人数'
+  attr: '検査実施件数'
   value: number
   children: [
     {
@@ -9,16 +9,6 @@ type DataType = {
         {
           attr: '入院中'
           value: number
-          children: [
-            {
-              attr: '軽症・中等症'
-              value: number
-            },
-            {
-              attr: '重症'
-              value: number
-            }
-          ]
         },
         {
           attr: '退院'
@@ -38,11 +28,9 @@ type DataType = {
 }
 
 type ConfirmedCasesType = {
-  検査実施人数: number
+  検査実施件数: number
   陽性者数: number
   入院中: number
-  軽症中等症: number
-  重症: number
   死亡: number
   退院: number
   調査中: number
@@ -55,11 +43,9 @@ type ConfirmedCasesType = {
  */
 export default (data: DataType) => {
   const formattedData: ConfirmedCasesType = {
-    検査実施人数: data.value,
+    検査実施件数: data.value,
     陽性者数: data.children[0].value,
     入院中: data.children[0].children[0].value,
-    軽症中等症: data.children[0].children[0].children[0].value,
-    重症: data.children[0].children[0].children[1].value,
     死亡: data.children[0].children[2].value,
     退院: data.children[0].children[1].value,
     調査中: data.children[0].children[3].value
